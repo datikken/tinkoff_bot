7f8012c92058adfacbd6fdcbe5a07fe1c9dd76a0
datikken <tikken23@gmail.com>
2023-04-14 22:03:57 +0300

    sql alchemy added

--- app/sql/models.py
+++ app/sql/models.py
@@ -6,9 +6,9 @@ class Order(Base):
   __tablename__ = 'orders'
   
   id = Column(Integer, primary_key=True, index=True)
-  order_id = Column(String)
-  figi = Column(String)
-  order_direction = Column(String)
+  order_id = Column(String(256))
+  figi = Column(String(256))
+  order_direction = Column(String(256))
   price = Column(Float)
   quantity = Column(Integer)
-  status = Column(String)
+  status = Column(String(256))
