3410f45502dbadd5f0c1131c320814dc3bea4274
datikken <tikken23@gmail.com>
2023-04-15 22:21:26 +0300

    remove dumb db, mysql added

---
+++ app/stats/sql_client.py
@@ -0,0 +1,38 @@
+from app.sql.models import Order
+from app.sql.database import db
+
+class StatsSQLClient:
+    def add_order(
+        self,
+        order_id: str,
+        figi: str,
+        order_direction: str,
+        price: float,
+        quantity: int,
+        status: str,
+    ):
+        order = Order(
+            order_id=order_id, 
+            figi=figi, 
+            order_direction=order_direction, 
+            price=price, 
+            quantity=quantity, 
+            status=status
+        )
+        db.add(order)
+        self.save()
+
+    def save(self):
+        db.flush()
+        db.commit()
+    
+    def get_orders(self):
+        return db.query(Order).all()
+
+    def update_order_status(self, order_id: str, status: str):
+        db.query(Order).filter(Order.order_id == order_id).update({ Order.status: status })
+        self.save()
+        # self.db_client.execute_update(
+        #     "UPDATE orders SET status=? WHERE id=?",
+        #     (status, order_id),
+        # )
