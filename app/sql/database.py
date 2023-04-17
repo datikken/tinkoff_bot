3a2790cfea10789427a3ac33c76fe447b15a77f1
datikken <shadybekov@cointelegraph.com>
2023-04-17 21:35:02 +0300

    sql refactored

--- app/sql/database.py
+++ app/sql/database.py
@@ -1,8 +1,7 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.settings import settings
-from sqlalchemy import Column, Integer, String, Float
+from app.sql.models import Base
 
 SQLALCHEMY_DATABASE_URL = f"mysql://{settings.db_user}:{settings.db_pass}@127.0.0.1:3306/{settings.db_name}"
 
@@ -11,20 +10,6 @@ engine = create_engine(
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-Base = declarative_base()
-
-class Order(Base):
-  __tablename__ = 'orders'
-  
-  id = Column(Integer, primary_key=True, index=True)
-  order_id = Column(String(256))
-  figi = Column(String(256))
-  order_direction = Column(String(256))
-  price = Column(Float)
-  quantity = Column(Integer)
-  status = Column(String(256))
-
 
 Base.metadata.create_all(bind=engine)
 
---
+++ app/sql/models.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, Integer, String, Float, DateTime
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Order(Base):
+  __tablename__ = 'orders'
+  __table_args__ = {'extend_existing': True}
+
+  id = Column(Integer, primary_key=True, index=True)
+  order_id = Column(String(256))
+  figi = Column(String(256))
+  order_direction = Column(String(256))
+  price = Column(Float)
+  quantity = Column(Integer)
+  status = Column(String(256))
+
+
+class Corridor(Base):
+  __tablename__ = 'corridors'
+  __table_args__ = {'extend_existing': True}
+
+  id = Column(Integer, primary_key=True, index=True)
+  bottom = Column(Float)
+  top = Column(Float)
+  current = Column(Float)
+  days_considered = Column(Integer)
+  figi = Column(String(256))
+  created_at = Column(DateTime)
+  updated_at = Column(DateTime)
