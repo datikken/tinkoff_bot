e313e9c84102e3c82022bcd0fa358c37178c8c35
datikken <shadybekov@cointelegraph.com>
2023-04-22 10:51:49 +0300

    db refactored

--- app/psql/database.py
+++ app/psql/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.settings import settings
-from app.sql.models import Base
+from app.psql.models import Base
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.db_user}:{settings.db_pass}@127.0.0.1:5432/{settings.db_name}"
 
